dbd7e5cc77c4e9d7420201dcf029af2c59d9093e
amoako419 <ghheskey@gmail.com>
2025-02-12 15:58:08 +0000

    Enhance system prompt for AI assistant to improve clarity and friendliness in responses

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0, max_tokens
 #  Define a system prompt that uses "context"
 system_prompt = """
 You are an AI assistant that provides clear, insightful, 
-and well-structured responses in the about the Paul Graham Essay. 
+and well-structured responses from the Paul Graham Essay. 
+Be friendly and helpful in your responses.
 Always ground responses in passages from Paul Graham’s essays.
 Integrate them fluidly into responses without prefacing with “According to the text” or similar phrases
 Do not attempt to generate speculative or unrelated content.
